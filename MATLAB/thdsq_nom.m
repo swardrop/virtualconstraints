11a134fe798071b3586a89e79e75487c84622749
swardrop <stephen.wardrop@gmail.com>
2014-09-24 22:58:40 +1000

    Working ConstrGui
    Just before I intend to go playing! Ha ha ha!

--- MATLAB/thdsq_nom.m
+++ MATLAB/thdsq_nom.m
@@ -8,10 +8,15 @@ delqd = impactMatrices(Phi_thf);
 [~,~,c] = constrMatrices;
 delthd = c*delqd*d_Phi_thf;
 
-% thdsq = cd.Psi_f/((delthd)^(-2) - cd.Gamma_f); Periodic
+% Periodic velocity
+% thdsq = cd.Psi_f/((delthd)^(-2) - cd.Gamma_f)
 
 % Fixed velocity after impact
-thdsq_p = 2.5;
-thdsq = ( (thdsq_p/delthd^2) - cd.Psi_f ) / cd.Gamma_f;
+% thdsq_p = 2.5;
+% thdsq = ( (thdsq_p/delthd^2) - cd.Psi_f ) / cd.Gamma_f;
+
+% Fixed factor over minimum feasible velocity after impact
+ratio = 1.2;
+thdsq = (delthd^(-2)*(-ratio^2*cd.Psi_c/cd.Gamma_c) - cd.Psi_f)/cd.Gamma_f;
 
 end
