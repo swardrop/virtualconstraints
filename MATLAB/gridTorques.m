2abde9e4855abdb0fb9e37ffea1df2df1547cf01
swardrop <stephen.wardrop@gmail.com>
2014-09-30 00:26:53 +1000

    Wrote up single VC optimisation

--- MATLAB/gridTorques.m
+++ MATLAB/gridTorques.m
@@ -1,25 +1,24 @@
 function [alpha2, alpha3, cost] = ...
     gridTorques(th_end, al_end, N, thd_sq_0, type)
 %gridTorques Takes a final control point of a Bezier curve constraint
-% and produces a NxN grid of the 2-norm of the torque resuling from the
+% and produces a NxN grid of the p-norm of the torque resulting from the
 % gridded alpha2 and alpha3 values.
 
-% Only relevant for a degree-5 constraint on the compass-gait robot
-
 % Set up constraint values
+al_p(:,6) = al_end;
 al_p(:,5) = al_end;
-th_p(5) = th_end;
-[b0, b_th0, b1] = invarianceCond(th_p, al_p, th_end, 5, true);
+th_p(6) = th_end;
+[b0, b_th0, b1] = invarianceCond(th_p, al_p, th_end, 6, true);
 th_p(1) = b_th0;
 al_p(:,1) = b0;
 al_p(:,2) = b1;
 
-cen2 = 2/4*(al_end-b0)+b0;
-min2 = cen2 - pi/2;
-max2 = cen2 + pi/2;
-cen3 = 3/4*(al_end-b0)+b0;
-min3 = cen3 - pi/2;
-max3 = cen3 + pi/2;
+cen2 = 2/5*(al_end-b0)+b0;
+min2 = cen2 - pi/1.5;
+max2 = cen2 + pi/1.5;
+cen3 = 3/5*(al_end-b0)+b0;
+min3 = cen3 - pi/1.5;
+max3 = cen3 + pi/1.5;
 
 alpha2 = linspace(min2, max2, N);
 alpha2 = repmat(alpha2, N, 1);
@@ -46,12 +45,12 @@ for i = 1 : size(alpha2, 1)
 end
 
 % Cut out any row or column which will blow out the scale
-inrange = cost < 3*median(cost(:));
-rowsInRange = sum(inrange,2);
-colsInRange = sum(inrange,1);
-alpha2 = removeOutOfRange(alpha2, rowsInRange, colsInRange, N);
-alpha3 = removeOutOfRange(alpha3, rowsInRange, colsInRange, N);
-cost = removeOutOfRange(cost, rowsInRange, colsInRange, N);
+% inrange = cost < 3*median(cost(:));
+% rowsInRange = sum(inrange,2);
+% colsInRange = sum(inrange,1);
+% alpha2 = removeOutOfRange(alpha2, rowsInRange, colsInRange, N);
+% alpha3 = removeOutOfRange(alpha3, rowsInRange, colsInRange, N);
+% cost = removeOutOfRange(cost, rowsInRange, colsInRange, N);
 
 figure;
 surfc(alpha2,alpha3,cost);
